7899467d01745e4c49641d5af111906f18ab7b9c
cyrox007 <cyrox007@gmail.com>
2022-08-09 20:50:59 +0300

    index update

--- scripts/main.js
+++ scripts/main.js
@@ -14,4 +14,17 @@ document.addEventListener("DOMContentLoaded", ()=>{
         mobileBtn.classList.toggle('active');
         mobileNav.classList.toggle('visible');
     });
+
+    let questions = document.querySelectorAll('.faq__question');
+    questions.forEach(elem => {
+        elem.addEventListener('click', ()=>{
+            elem.querySelector('img').classList.toggle('active');
+            let panel = elem.querySelector('.faq__answer');
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    });
 });
